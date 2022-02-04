fe3d7e0114998d895c7cf0e6e4ba33b8813c0e71
jairoandresm <jaanmorales@utp.edu.co>
2022-02-03 21:25:17 -0500

    clase de funciones globales

--- primeros_pasos.sol
+++ primeros_pasos.sol
@@ -1,6 +1,23 @@
 //pragma solidity ^0.4.0;
+//indicamos el rango de compiladores de nuestro proyecto
 pragma solidity >=0.4.4 <0.0.7;
 
+//importar archivo ERC20.sol
+import "./ERC20.sol"
+
+//@notice explicar lo que hace el contrato
+//nombre de contrato
 contract PrimerContrato{
-    
+// la variable owner es la direccion de la persona que despliega el contrato 
+
+    address owner;
+
+// inicializamos el numero de tokens
+    ERC20Basic token;
+
+    constructor () public {
+        owner = msg.sender;
+        token = new ERC20Basic(1000);
+    }
+
 }
