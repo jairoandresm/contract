95dcb0fb793ee0e8bf791e7ca48d4f981a571dcd
jairoandresm <jaanmorales@utp.edu.co>
2022-02-03 21:10:17 -0500

    estandar de comentarios

---
+++ comentarios.sol
@@ -0,0 +1,8 @@
+/*
+@tittle <titulo de comentario>
+@author <Autor del contrato>
+@notice <explica lo que hace el contrato>
+@dev <detalles adicionales del contrato>
+@param <nombre del parametro, describe para que sirve el parametro>
+@return <valor retorno, describe para que sirve el valor retornado>
+*/
