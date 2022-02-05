a832c9bb879b1e2464a55b473b4377c50ce77ac7
jairoandresm <jaanmorales@utp.edu.co>
2022-02-05 12:25:38 -0500

    variables en solidity

---
+++ 3_variables/variables_enteras.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.4 <0.7.0;
+
+
+contract enteros{
+    //variables enteras sin signo
+    uint mi_primer_entero;
+    uint mi_primer_entero_inicializado = 3;
+    uint cota = 5000;
+
+    //variables enteras sin signo con numero de bits
+    uint8 entero_8_bits;
+    uint64 entero_64_bits = 7000;
+
+    //variables enteras con signo
+    int mi_primer_entero_con_signo;
+    int mi_numero = -32;
+    int mi_numero_2 = 65;
+
+
+
+} 
---
+++ apuntes.sol
@@ -0,0 +1,32 @@
+/* apuntes solidity
+
+para declarar vatiables enteras hay que tener en cuenta cuando estas tienen signo
+la forma de declarar una variable es:
+
+<type> <nombre de la variable> = <valor de la variable>
+
+variables enteras sin signo <uint>;
+variables enteras con signo <int>;
+
+uint<x> donde x es el numero de bits, varia de 8 a 256 en multiplos de 8
+
+
+//booleanos
+
+bool nombre_variable;
+bool nombre_variable = true/false;
+
+bytes<x> <nombre_variable>;
+x varia entre 1 a 32
+
+si se usa byte se entiende como bytes1
+
+address <nombre_variable>;
+variable es la direccion de la cuenta en ethereum
+
+
+
+
+
+
+*/
